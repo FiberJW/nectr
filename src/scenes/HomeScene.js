8fc9edee938a53331b5729de6e15e101163aaf32
Juwan Wheatley <datwheat@gmail.com>
2016-10-01 20:18:20 -0400

    major key

--- src/scenes/HomeScene.js
+++ src/scenes/HomeScene.js
@@ -1,48 +1,13 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  Dimensions,
-  ToolbarAndroid,
-  StyleSheet,
-  StatusBar,
-} from 'react-native';
 
-import colors from '../config/colors.js';
-
-const styles = StyleSheet.create({
-  toolbar: {
-    backgroundColor: colors.primaryBlue,
-    height: 56,
-    borderColor: 'red',
-    borderWidth: 1,
-  },
-});
+import DrawerNavLayout from '../components/DrawerNavLayout';
 
 export default class HomeScene extends Component {
-  onActionSelected(position) {
-    if (position === 0) { // index of 'Settings'
-      showSettings();
-    }
-  }
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          paddingTop: StatusBar.currentHeight,
-          backgroundColor: '#ffffff',
-        }}
-      >
-        <StatusBar backgroundColor="rgba(0, 0, 0, 0.5)" />
-        <ToolbarAndroid
-          navIcon={require('nectr/src/images/menu_icon.png')}
-          title="Feed"
-          titleColor="#ffffff"
-          style={styles.toolbar}
-          // actions={[{title: '', show: 'always'}]}
-          onActionSelected={this.onActionSelected} />
-      </View>
+      <DrawerNavLayout title="Feed" sceneId="feed">
+        
+      </DrawerNavLayout>
     )
   }
 }
